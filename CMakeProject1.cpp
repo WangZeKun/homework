a6bedab3921e20882b4fc207f1864152b8f99a1a
zekin <wzekin@gmail.com>
2019-03-10 12:22:36 +0800

    init cmake project

---
+++ CMakeProject1.cpp
@@ -0,0 +1,17 @@
+﻿// CMakeProject1.cpp: 定义应用程序的入口点。
+//
+
+#include "CMakeProject1.h"
+
+using namespace std;
+
+int main()
+{
+	string a;
+	string b;
+	cin >> a;
+	cout << a<< endl;
+	cin >> b;
+	cout << b<< endl;
+	return 0;
+}
